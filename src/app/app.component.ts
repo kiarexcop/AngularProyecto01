e10f9b09f59f7d9470472786e43fa715879ac11e
kiarexcop <019101873k@uandina.edu.pe>
2022-04-06 10:25:02 -0700

    ya casi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { comentario, disponibilidad } from './interfaces/disponibilidad';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto01';
+  //title = 'proyecto01';
+  public producto:disponibilidad [ ] = [ ];
+
+  public pro01 : disponibilidad={
+    
+    tiendas: "Funkomania Odrem",
+    cantidad: "3 disponibles",
+    direccion: "av.sol 123 alltura del banco credinka",
+    ciudad: "cusco"
+  };
+  public pro02 : disponibilidad={
+    
+    tiendas: "Funkomania Stras",
+    cantidad: "5 disponibles",
+    direccion: "san jeronimo 312 4to paradero",
+    ciudad: "cusco"
+  };
+  public pro03 : disponibilidad={
+    
+    tiendas: "Funkomania Oficial",
+    cantidad: "10 disponibles",
+    direccion: "ovalo rimac 456",
+    ciudad: "lima"
+  };
+  onDatos():void{
+    /*console.log(this.pro01)
+    console.log(this.pro02)
+    console.log(this.pro03)*/
+    this.producto.push(this.pro01)
+    this.producto.push(this.pro02)
+    this.producto.push(this.pro03)
+  }
+  /*onGUardar():void{
+    console.log("metodo onGUardar() invocado");
+
+  }*/
+
+  public comen:comentario={
+    comenta:""
+
+  }
+  onDatos2():void{
+    console.log(this.comen)
+
+  }
+  onChange(event: Event){
+    const elemento = event.target as HTMLInputElement;
+    this.comen.comenta = elemento.value;
+
+  }
 }
---
+++ src/app/interfaces/disponibilidad.ts
@@ -0,0 +1,11 @@
+export interface disponibilidad {
+    
+    tiendas: string;
+    cantidad: string;
+    direccion: string;
+    ciudad: string;
+
+}
+export interface comentario{
+    comenta: string;
+}
